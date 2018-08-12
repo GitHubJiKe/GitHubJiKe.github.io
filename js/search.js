ce5a206ad89c289a8770459ad661e608330c77f1
peter.yuan <pengfei.yuan@boldseas.com>
2018-08-12 10:26:12 +0800

    post

--- js/search.js
+++ js/search.js
@@ -22,7 +22,7 @@
 // Pieter Robberechts <http://github.com/probberechts>
 
 /*exported searchFunc*/
-var searchFunc = function(path, searchId, contentId) {
+var searchFunc = function (path, searchId, contentId) {
 
   function stripHtml(html) {
     html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
@@ -42,9 +42,9 @@ var searchFunc = function(path, searchId, contentId) {
     var i, j, result = [];
 
     for (i = 0; i < keywords.length; i++) {
-        for (j = i + 1; j < keywords.length + 1; j++) {
-            result.push(keywords.slice(i, j).join(" "));
-        }
+      for (j = i + 1; j < keywords.length + 1; j++) {
+        result.push(keywords.slice(i, j).join(" "));
+      }
     }
     return result;
   }
@@ -52,13 +52,13 @@ var searchFunc = function(path, searchId, contentId) {
   $.ajax({
     url: path,
     dataType: "xml",
-    success: function(xmlResponse) {
+    success: function (xmlResponse) {
       // get the contents from search data
-      var datas = $("entry", xmlResponse).map(function() {
+      var datas = $("entry", xmlResponse).map(function () {
         return {
           title: $("title", this).text(),
           content: $("content", this).text(),
-          url: $("link", this).attr("href")
+          url: decodeURIComponent($("url", this).text())
         };
       }).get();
 
@@ -66,16 +66,16 @@ var searchFunc = function(path, searchId, contentId) {
       if (!$input) { return; }
       var $resultContent = document.getElementById(contentId);
 
-      $input.addEventListener("input", function(){
+      $input.addEventListener("input", function () {
         var resultList = [];
         var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
-          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
+          .sort(function (a, b) { return b.split(" ").length - a.split(" ").length; });
         $resultContent.innerHTML = "";
         if (this.value.trim().length <= 0) {
           return;
         }
         // perform local searching
-        datas.forEach(function(data) {
+        datas.forEach(function (data) {
           var matches = 0;
           if (!data.title || data.title.trim() === "") {
             data.title = "Untitled";
@@ -88,11 +88,11 @@ var searchFunc = function(path, searchId, contentId) {
           var firstOccur = -1;
           // only match artiles with not empty contents
           if (dataContent !== "") {
-            keywords.forEach(function(keyword) {
+            keywords.forEach(function (keyword) {
               indexTitle = dataTitle.indexOf(keyword);
               indexContent = dataContent.indexOf(keyword);
 
-              if( indexTitle >= 0 || indexContent >= 0 ){
+              if (indexTitle >= 0 || indexContent >= 0) {
                 matches += 1;
                 if (indexContent < 0) {
                   indexContent = 0;
@@ -107,21 +107,21 @@ var searchFunc = function(path, searchId, contentId) {
           if (matches > 0) {
             var searchResult = {};
             searchResult.rank = matches;
-            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
+            searchResult.str = "<li><a href='" + dataUrl + "' class='search-result-title'>" + dataTitle + "</a>";
             if (firstOccur >= 0) {
               // cut out 100 characters
               var start = firstOccur - 20;
               var end = firstOccur + 80;
 
-              if(start < 0){
+              if (start < 0) {
                 start = 0;
               }
 
-              if(start == 0){
+              if (start == 0) {
                 end = 100;
               }
 
-              if(end > dataContent.length){
+              if (end > dataContent.length) {
                 end = dataContent.length;
               }
 
@@ -129,20 +129,20 @@ var searchFunc = function(path, searchId, contentId) {
 
               // highlight all keywords
               var regS = new RegExp(keywords.join("|"), "gi");
-              matchContent = matchContent.replace(regS, function(keyword) {
-                return "<em class=\"search-keyword\">"+keyword+"</em>";
+              matchContent = matchContent.replace(regS, function (keyword) {
+                return "<em class=\"search-keyword\">" + keyword + "</em>";
               });
 
-              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
+              searchResult.str += "<p class=\"search-result\">" + matchContent + "...</p>";
             }
             searchResult.str += "</li>";
             resultList.push(searchResult);
           }
         });
-        resultList.sort(function(a, b) {
-            return b.rank - a.rank;
+        resultList.sort(function (a, b) {
+          return b.rank - a.rank;
         });
-        var result ="<ul class=\"search-result-list\">";
+        var result = "<ul class=\"search-result-list\">";
         for (var i = 0; i < resultList.length; i++) {
           result += resultList[i].str;
         }
